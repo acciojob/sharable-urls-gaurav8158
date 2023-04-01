5f0bf4fe21a276af6571b7f7ed06fd2907d22dc5
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-01 22:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+document.getElementById("button").addEventListener("click", function() {
+			var name = document.getElementById("name").value;
+			var year = document.getElementById("year").value;
+			var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+			document.getElementById("url").textContent = url;
+		});
